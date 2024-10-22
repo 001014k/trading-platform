6097bafd500486e7023e219cb28cc0d5ed07a736
Kmj <hm4854@gmail.com>
2024-10-22 15:36:51 +0900

    20241022 (각 사용자마다 입찰 금액을 적어서 상세 제품 페이지에 시각화 기능 구현)

--- lib/Productdetail_Page.dart
+++ lib/Productdetail_Page.dart
@@ -31,7 +31,7 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
   bool isInWishlist = false;
   int viewCount = 0;
   String? userEmail;
-  String? currentUserId; //현재 로그인한 사용자의 id
+  String? currentUserId; // 현재 로그인한 사용자의 ID
 
   @override
   void initState() {
@@ -54,14 +54,16 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
 
   Future<void> fetchUserEmail() async {
     // 1. products 컬렉션에서 제품 고유 번호로 해당 제품 문서를 가져옴
-    final productSnapshot = await _firestore.collection('products').doc(widget.id).get();
+    final productSnapshot =
+        await _firestore.collection('products').doc(widget.id).get();
 
     if (productSnapshot.exists) {
       // 2. 해당 제품 문서에서 userId 필드를 가져옴
       String userId = productSnapshot.data()?['userId'];
 
       // 3. 가져온 userId를 사용하여 users 컬렉션에서 판매자의 이메일을 조회
-      final userSnapshot = await _firestore.collection('users').doc(userId).get();
+      final userSnapshot =
+          await _firestore.collection('users').doc(userId).get();
       if (userSnapshot.exists) {
         setState(() {
           userEmail = userSnapshot.data()?['email']; // 판매자의 이메일을 저장
@@ -97,7 +99,6 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
   }
 
   Future<void> toggleWishlist() async {
-    // 로그인한 사용자의 userId를 가져옴 (currentUserId 사용)
     final userWishlistRef = _firestore
         .collection('users')
         .doc(currentUserId) // widget.userId 대신 currentUserId 사용
@@ -127,6 +128,68 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
     }
   }
 
+  // 사용자가 입력한 금액을 Firestore에 저장하는 함수
+  Future<void> submitUserPrice(String userPriceInput) async {
+    if (userPriceInput.isNotEmpty && currentUserId != null) {
+      String? email =
+          FirebaseAuth.instance.currentUser?.email; // 현재 사용자의 이메일 가져오기
+      if (email != null) {
+        // 이메일이 null이 아닐 때만 저장
+        await _firestore
+            .collection('products')
+            .doc(widget.id)
+            .collection('offers')
+            .add({
+          'userId': currentUserId,
+          'userEmail': email, // 사용자의 이메일 추가
+          'price': double.tryParse(userPriceInput) ?? 0.0,
+          'createdAt': FieldValue.serverTimestamp(),
+        });
+      } else {
+        print("사용자 이메일을 가져오는 데 실패했습니다.");
+      }
+    } else {
+      print("입찰 금액이나 사용자 ID가 null입니다.");
+    }
+  }
+
+  void _showBidDialog() {
+    String userPriceInput = ""; // 대화 상자에서 사용할 입력값
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('입찰 금액 입력'),
+          content: TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: '입찰 금액',
+              border: OutlineInputBorder(),
+            ),
+            onChanged: (value) {
+              userPriceInput = value; // 입력한 금액 저장
+            },
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                submitUserPrice(userPriceInput); // 입력된 금액 제출
+                Navigator.of(context).pop(); // 대화 상자 닫기
+              },
+              child: Text('제출'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // 대화 상자 닫기
+              },
+              child: Text('취소'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -157,7 +220,8 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
           ),
         ],
       ),
-      body: Padding(
+      body: SingleChildScrollView(
+        // SingleChildScrollView로 감싸기
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -165,12 +229,13 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
             widget.imageUrl.isNotEmpty
                 ? Image.network(widget.imageUrl, fit: BoxFit.cover)
                 : Container(
-              height: 200,
-              color: Colors.grey[200],
-              child: Icon(Icons.image_not_supported, size: 100),
-            ),
+                    height: 200,
+                    color: Colors.grey[200],
+                    child: Icon(Icons.image_not_supported, size: 100),
+                  ),
             SizedBox(height: 16),
-            Text(widget.name, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            Text(widget.name,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
             Divider(thickness: 1, color: Colors.grey[300]),
             SizedBox(height: 8),
             Text(widget.description, style: TextStyle(fontSize: 16)),
@@ -178,21 +243,26 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
             SizedBox(height: 8),
             Row(
               children: [
-                Text('키워드:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                Text('키워드:',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
                 SizedBox(width: 8),
-                Text(widget.keyword, style: TextStyle(fontSize: 16, color: Colors.grey[700])),
+                Text(widget.keyword,
+                    style: TextStyle(fontSize: 16, color: Colors.grey[700])),
               ],
             ),
             Divider(thickness: 1, color: Colors.grey[300]),
             SizedBox(height: 8),
             Row(
               children: [
-                Text('₩$formattedPrice', style: TextStyle(fontSize: 20, color: Colors.blueAccent)),
+                Text('₩$formattedPrice',
+                    style: TextStyle(fontSize: 20, color: Colors.blueAccent)),
               ],
             ),
             SizedBox(height: 16),
             if (userEmail != null) ...[
-              Text('판매자 이메일: $userEmail', style: TextStyle(fontSize: 16, color: Colors.grey[700])),
+              Text('판매자 이메일: $userEmail',
+                  style: TextStyle(fontSize: 16, color: Colors.grey[700])),
               SizedBox(height: 8),
             ],
             Row(
@@ -200,13 +270,69 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
               children: [
                 Row(
                   children: [
-                    Icon(isInWishlist ? Icons.favorite : Icons.favorite_border, color: isInWishlist ? Colors.red : Colors.grey),
+                    Icon(isInWishlist ? Icons.favorite : Icons.favorite_border,
+                        color: isInWishlist ? Colors.red : Colors.grey),
                     SizedBox(width: 8),
-                    Text(isInWishlist ? '위시리스트에 추가됨' : '위시리스트에 추가', style: TextStyle(fontSize: 16)),
+                    Text(isInWishlist ? '위시리스트에 추가됨' : '위시리스트에 추가',
+                        style: TextStyle(fontSize: 16)),
                   ],
                 ),
                 Text('조회수: $viewCount', style: TextStyle(fontSize: 16)),
               ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center, // 중앙 정렬
+              children: [
+                ElevatedButton(
+                  onPressed: _showBidDialog,
+                  child: Text('입찰 금액 입력'),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+
+            SizedBox(height: 16),
+            // 사용자의 입찰 내역 표시
+            StreamBuilder<QuerySnapshot>(
+              stream: _firestore
+                  .collection('products')
+                  .doc(widget.id)
+                  .collection('offers')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return CircularProgressIndicator();
+                }
+
+                return Column(
+                  children: snapshot.data!.docs.map((offer) {
+                    final offerData =
+                        offer.data() as Map<String, dynamic>?; // 안전하게 타입 변환
+                    final offerUserEmail =
+                        offerData?['userEmail'] ?? '알 수 없는 사용자'; // 이메일 출력
+                    final offerPrice = offerData?['price'] != null
+                        ? '₩${NumberFormat('#,##0').format(offerData!['price'])}' // 금액 포맷팅
+                        : '금액 없음'; // null 체크
+
+                    return Container(
+                      padding: EdgeInsets.all(8),
+                      margin: EdgeInsets.symmetric(vertical: 4),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(offerUserEmail, style: TextStyle(fontSize: 16)),
+                          // 이메일 표시
+                          Text(offerPrice, style: TextStyle(fontSize: 16)),
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                );
+              },
             ),
           ],
         ),
