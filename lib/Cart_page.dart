3a8be3c497947cd8e0af7f4cf568cfa827888f8e
Kmj <hm4854@gmail.com>
2024-10-23 17:27:14 +0900

    20241023 (가격 표시를 좀 더 정확하게 시각화 구현)

--- lib/Cart_page.dart
+++ lib/Cart_page.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class CartPage extends StatefulWidget {
-  final String userId; // 사용자의 ID를 전달받기 위한 변수
+  final String userId;
 
-  CartPage({required this.userId}); // 생성자에서 사용자 ID를 받도록 설정
+  CartPage({required this.userId});
 
   @override
   _CartPageState createState() => _CartPageState();
 }
 
 class _CartPageState extends State<CartPage> {
-  List<String> selectedItems = []; // 선택된 항목 ID를 저장할 리스트
-  double totalPrice = 0.0; // 총 가격 변수
+  List<String> selectedItems = [];
+  double totalPrice = 0.0;
 
   Future<void> deleteItem(String docId) async {
     try {
@@ -20,11 +21,17 @@ class _CartPageState extends State<CartPage> {
           .collection('users')
           .doc(widget.userId)
           .collection('cart')
-          .doc(docId) // 삭제할 문서 ID
-          .delete(); // 문서 삭제
+          .doc(docId)
+          .delete();
     } catch (e) {
       print('삭제 오류: $e');
     }
+  }
+
+  // 가격을 포맷팅하는 함수
+  String formattedPrice(double price) {
+    final formatter = NumberFormat.currency(locale: 'ko_KR', symbol: '₩', decimalDigits: 0);
+    return formatter.format(price);
   }
 
   @override
@@ -67,17 +74,17 @@ class _CartPageState extends State<CartPage> {
                   itemBuilder: (context, index) {
                     final item = items[index];
                     return Container(
-                      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0), // 간격 조절
-                      padding: EdgeInsets.all(16.0), // 내부 여백
+                      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                      padding: EdgeInsets.all(16.0),
                       decoration: BoxDecoration(
                         color: Colors.white,
-                        borderRadius: BorderRadius.circular(8.0), // 둥근 모서리
+                        borderRadius: BorderRadius.circular(8.0),
                         boxShadow: [
                           BoxShadow(
-                            color: Colors.grey.withOpacity(0.2), // 그림자 색상
+                            color: Colors.grey.withOpacity(0.2),
                             spreadRadius: 1,
                             blurRadius: 5,
-                            offset: Offset(0, 3), // 위치 조절
+                            offset: Offset(0, 3),
                           ),
                         ],
                       ),
@@ -104,13 +111,12 @@ class _CartPageState extends State<CartPage> {
                                 style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
                               ),
                               subtitle: Text(
-                                '가격: ₩${item['productPrice'].toString()}',
+                                '가격: ${formattedPrice(item['productPrice'])}', // 포맷된 가격 표시
                                 style: TextStyle(color: Colors.black),
                               ),
                               trailing: IconButton(
                                 icon: Icon(Icons.delete, color: Colors.red),
                                 onPressed: () {
-                                  // 삭제 버튼 클릭 시
                                   showDialog(
                                     context: context,
                                     builder: (context) {
@@ -122,16 +128,9 @@ class _CartPageState extends State<CartPage> {
                                         content: Text('이 항목을 삭제하시겠습니까?'),
                                         actions: [
                                           TextButton(
-                                            style: ElevatedButton.styleFrom(
-                                              backgroundColor: Colors.blue,
-                                              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-                                              shape: RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.circular(8), // 둥근 모서리 설정
-                                              ),
-                                            ),
                                             onPressed: () {
-                                              deleteItem(item.id); // 항목 삭제
-                                              Navigator.of(context).pop(); // 대화 상자 닫기
+                                              deleteItem(item.id);
+                                              Navigator.of(context).pop();
                                             },
                                             child: Text(
                                               '삭제',
@@ -139,15 +138,8 @@ class _CartPageState extends State<CartPage> {
                                             ),
                                           ),
                                           TextButton(
-                                            style: ElevatedButton.styleFrom(
-                                              backgroundColor: Colors.blue,
-                                              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-                                              shape: RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.circular(8), // 둥근 모서리 설정
-                                              ),
-                                            ),
                                             onPressed: () {
-                                              Navigator.of(context).pop(); // 대화 상자 닫기
+                                              Navigator.of(context).pop();
                                             },
                                             child: Text(
                                               '취소',
@@ -178,20 +170,18 @@ class _CartPageState extends State<CartPage> {
                       children: [
                         Text(
                           '총 가격',
-                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold), // 총 가격 타이틀
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-                        SizedBox(height: 8), // 타이틀과 총 가격 사이의 간격
+                        SizedBox(height: 8),
                         Text(
-                          '₩${totalPrice.toStringAsFixed(0)}', // 총 가격 표시
+                          formattedPrice(totalPrice), // 포맷된 총 가격 표시
                           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                         ),
                       ],
                     ),
                     ElevatedButton(
                       onPressed: () {
-                        // 결제하기 버튼 클릭 시의 로직
                         if (selectedItems.isNotEmpty) {
-                          // 결제 로직 구현
                           showDialog(
                             context: context,
                             builder: (context) {
@@ -202,14 +192,13 @@ class _CartPageState extends State<CartPage> {
                                   TextButton(
                                     onPressed: () {
                                       // 결제 처리 로직 추가
-                                      // 예: Firestore에 결제 정보 저장 등
-                                      Navigator.of(context).pop(); // 대화 상자 닫기
+                                      Navigator.of(context).pop();
                                     },
                                     child: Text('결제하기'),
                                   ),
                                   TextButton(
                                     onPressed: () {
-                                      Navigator.of(context).pop(); // 대화 상자 닫기
+                                      Navigator.of(context).pop();
                                     },
                                     child: Text('취소'),
                                   ),
